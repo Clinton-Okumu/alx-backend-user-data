1b728a40050d36263ca361c21839298fd40aa7a9
Clinton-Okumu <clintonomondiokumu@gmail.com>
2024-11-21 21:02:18 +0300

    0x03-user_authentication_service

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
-from sqlalchemy import Column, Integer, String
+"""
+Defines the User model for a SQLAlchemy ORM mapping to the 'users' table.
+"""
+
+from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class User(Base):
+    """
+    User model mapped to the 'users' table with attributes:
+    """
     __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
     reset_token = Column(String(250), nullable=True)
+
+
+if __name__ == "__main__":
+    engine = create_engine('sqlite:///a.db:', echo=True)
+    Base.metadata.create_all(engine)
